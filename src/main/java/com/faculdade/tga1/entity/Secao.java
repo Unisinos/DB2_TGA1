5fe2990a65b49b76fd25d8b08e7792cf09e767a1
paulograbin <plgrabin@gmail.com>
2014-03-10 20:04:43 -0300

    Adicionando coluna
    Adicionei a coluna nome.

--- src/main/java/com/faculdade/tga1/entity/Secao.java
+++ src/main/java/com/faculdade/tga1/entity/Secao.java
@@ -1,9 +1,14 @@
 package com.faculdade.tga1.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.*;
 
 /**
  * Created by iuriandreazza on 07/03/14.
@@ -12,7 +17,10 @@ import javax.persistence.*;
 @SequenceGenerator(name = "seq_id_Secao", sequenceName = "seq_id_Secao")
 public class Secao extends AbstractEntity<Long> {
 
-
+	@Getter
+    @Setter
+	private String nome;
+	
     @Getter
     @Setter
     @ManyToOne(fetch = FetchType.LAZY)
