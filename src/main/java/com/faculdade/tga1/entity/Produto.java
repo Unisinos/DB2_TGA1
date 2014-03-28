bc9a2b917b41a3f662e68f81e7f9efeb7c13801f
paulograbin <plgrabin@gmail.com>
2014-03-28 19:25:38 -0300

    Ajustando entidade
    Faltava relacionamento da Produto pra EstoqueGradeItem.

--- src/main/java/com/faculdade/tga1/entity/Produto.java
+++ src/main/java/com/faculdade/tga1/entity/Produto.java
@@ -1,12 +1,15 @@
 package com.faculdade.tga1.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.util.ArrayList;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @SequenceGenerator(name = "seq_id_Produto", sequenceName = "seq_id_Produto")
@@ -24,17 +27,18 @@ public class Produto extends AbstractEntity<Long> {
     @Setter
     private int estoqueMinimo;
 
-
     @Getter
     @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     private Categoria categoria;
-
 
     @Getter
     @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     private Grade grade;
 
-
+    @Getter
+    @Setter
+    @OneToMany(mappedBy="produto")
+    private ArrayList<EstoqueGradeItem> estoqueGradeItem;
 }
